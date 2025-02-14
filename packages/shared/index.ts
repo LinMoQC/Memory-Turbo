329d1fe9a8dea33ceb264ee292ad4fb368a6f220
linmo <huangkai04@bilibili.com>
2025-02-14 16:41:53 +0800

    feat: 合并至turbo repo大仓

---
+++ packages/shared/index.ts
@@ -0,0 +1,20 @@
+// 权限枚举
+export enum Roles {
+    public = 1,
+    admin = 2,
+}
+
+// 模版状态的枚举
+export enum templateStatusEnum {
+    DRAFT = "draft",
+    PENDING = "pending",
+    APPROVED = "approved",
+    REJECTED = "rejected",
+}
+
+// 用户账号状态的枚举
+export enum UserStatusEnum {
+    DISABLED = 0,    // 禁用
+    ACTIVE = 1,      // 正常
+}
+
